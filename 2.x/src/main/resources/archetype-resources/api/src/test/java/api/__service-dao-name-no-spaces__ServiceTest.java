4d0d1d62e42179e895a68254fb139615d4f3a8a6
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2014-08-21 18:08:12 +0200

    SDK-25 Added archetype for 2.x module

---
+++ 2.x/src/main/resources/archetype-resources/api/src/test/java/api/__service-dao-name-no-spaces__ServiceTest.java
@@ -0,0 +1,33 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package ${package}.api;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openmrs.api.context.Context;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+/**
+ * Tests {@link ${symbol_dollar}{${service-dao-name-no-spaces}Service}}.
+ */
+public class  ${service-dao-name-no-spaces}ServiceTest extends BaseModuleContextSensitiveTest {
+	
+	@Test
+	public void shouldSetupContext() {
+		assertNotNull(Context.getService(${service-dao-name-no-spaces}Service.class));
+	}
+}
